7d40e19bdb57f7584f47098dbaed6b64e1dc68c4
jonuchauhan <34107866+jonuchauhan@users.noreply.github.com>
2021-06-15 23:26:50 +0300

    flow control

---
+++ FlowControl.py
@@ -0,0 +1,19 @@
+"""IF CONTROL"""
+
+a = int(input("Enter any number"))
+
+if a % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
+
+"ELIF"
+
+a = int(input("Enter any Number"))
+
+if a == 0:
+    print("ZERO")
+elif a % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
